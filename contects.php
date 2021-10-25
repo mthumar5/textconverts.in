dd0d2cd3a46849b2c67ef1bf651dbe83ee2deb20
mehuP <mehulthumar@destiny.ws>
2021-10-26 00:04:33 +0530

    live 1
    live 1

---
+++ contects.php
@@ -0,0 +1,503 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><meta name="google-site-verification" content="vv5DtoLkprx_g1QF2qCKllWzrzEZmrg8M5xrGdvXLqs" />
+  
+  <meta charset="utf-8"> 
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <title>Text Converts | Contect Us</title>
+      <meta name="description" content="Easily text converts  between different letter cases: lower case, UPPER CASE, Capitalized Case, Sentence case, aLtErNaTiNg cAsE and more online.">
+      <meta name="viewport" content="width=device-width,initial-scale=1">
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="apple-touch-icon" sizes="57x57" href="img/apple-icon-57x57.png">
+      <link rel="apple-touch-icon" sizes="60x60" href="img/apple-icon-60x60.png">
+      <link rel="apple-touch-icon" sizes="72x72" href="img/apple-icon-72x72.png">
+      <link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon-76x76.png">
+      <link rel="apple-touch-icon" sizes="114x114" href="img/apple-icon-114x114.png">
+      <link rel="apple-touch-icon" sizes="120x120" href="img/apple-icon-120x120.png">
+      <link rel="apple-touch-icon" sizes="144x144" href="img/apple-icon-144x144.png">
+      <link rel="apple-touch-icon" sizes="152x152" href="img/apple-icon-152x152.png">
+      <link rel="apple-touch-icon" sizes="180x180" href="img/apple-icon-180x180.png">
+      <link rel="icon" type="image/png" sizes="192x192"  href="img/android-icon-192x192.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="96x96" href="img/favicon-96x96.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+      <link rel="manifest" href="/manifest.json">
+      <meta name="msapplication-TileColor" content="#ffffff">
+      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+      <meta name="theme-color" content="#ffffff">
+
+
+<link href='https://fonts.googleapis.com/css?family=Lato:400,100,300' rel='stylesheet' type='text/css'>
+
+<link href="https://fonts.googleapis.com/css?family=Raleway:100,400,700" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css?family=Oswald:200,400,700" rel="stylesheet">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+
+<script src="https://use.fontawesome.com/20ab91acc4.js"></script>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+ <style>
+  body {
+  margin: 0;
+  padding: 0;
+}
+
+#contact {
+  width: 100%;
+  height: 100vh;
+  background-color: #111;
+  overflow: hidden;
+  padding-bottom: 200px;
+}
+
+.section-header {
+  text-align: center;
+  margin: 0 auto;
+  padding-top: 50px;
+  margin-bottom: 50px;
+  font: 300 60px "Oswald", sans-serif;
+  letter-spacing: 6px;
+  color: #fff;
+  margin-bottom: 25px;
+}
+
+.contact-wrapper {
+  margin: 0 auto;
+  padding-top: 20px;
+  position: relative;
+  max-width: 800px;
+}
+
+/* Begin Left Contact Page */
+.form-horizontal {
+  float: left;
+  max-width: 400px;
+  font-family: "Lato";
+  font-weight: 400;
+}
+
+.form-control,
+textarea {
+  max-width: 400px;
+  background-color: #111;
+  color: #fff;
+  letter-spacing: 1px;
+}
+
+.send-button {
+  margin-top: 15px;
+  height: 34px;
+  width: 400px;
+  overflow: hidden;
+  transition: all 0.2s ease-in-out;
+}
+
+.button {
+  width: 400px;
+  height: 34px;
+  transition: all 0.2s ease-in-out;
+}
+
+.send-text {
+  display: block;
+  margin-top: 10px;
+  font: 300 14px "Lato", sans-serif;
+  letter-spacing: 2px;
+}
+
+.button:hover {
+  transform: translate3d(0px, -29px, 0px);
+}
+
+/* Begin Right Contact Page */
+.direct-contact-container {
+  max-width: 400px;
+  float: right;
+  margin-top: 5px;
+}
+
+/* Location, Phone, Email Section */
+.contact-list {
+  list-style-type: none;
+  margin-left: -30px;
+  padding-right: 20px;
+}
+
+.list-item {
+  line-height: 4;
+  color: #aaa;
+}
+
+.contact-text {
+  font: 300 18px "Lato", sans-serif;
+  letter-spacing: 1.9px;
+  color: #bbb;
+}
+
+.place {
+  margin-left: 62px;
+}
+
+.phone {
+  margin-left: 56px;
+}
+
+.gmail {
+  margin-left: 53px;
+}
+
+.contact-text a {
+  color: #bbb;
+  text-decoration: none;
+  transition-duration: 0.2s;
+}
+
+.contact-text a:hover {
+  color: #fff;
+  text-decoration: none;
+}
+
+/* Social Media Icons */
+.social-media-list {
+  position: relative;
+  font-size: 2.3rem;
+  text-align: center;
+  width: 100%;
+}
+
+.social-media-list li a {
+  color: #fff;
+}
+
+.social-media-list li {
+  position: relative;
+  top: 0;
+  left: -20px;
+  display: inline-block;
+  height: 70px;
+  width: 70px;
+  margin: 10px auto;
+  line-height: 70px;
+  border-radius: 50%;
+  color: #fff;
+  background-color: rgb(27, 27, 27);
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+}
+
+.social-media-list li:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 70px;
+  height: 70px;
+  line-height: 70px;
+  border-radius: 50%;
+  opacity: 0;
+  box-shadow: 0 0 0 1px #fff;
+  transition: all 0.2s ease-in-out;
+}
+
+.social-media-list li:hover {
+  background-color: #fff;
+}
+
+.social-media-list li:hover:after {
+  opacity: 1;
+  transform: scale(1.12);
+  transition-timing-function: cubic-bezier(0.37, 0.74, 0.15, 1.65);
+}
+
+.social-media-list li:hover a {
+  color: #111;
+}
+
+.copyright {
+  font: 200 14px "Oswald", sans-serif;
+  color: #555;
+  letter-spacing: 1px;
+  text-align: center;
+}
+
+hr {
+  border-color: rgba(255, 255, 255, 0.8);
+}
+
+/* Begin Media Queries*/
+@media screen and (max-width: 760px) {
+  #contact {
+    height: 1000px;
+  }
+  .section-header {
+    font-size: 65px;
+  }
+  .direct-contact-container,
+  .form-horizontal {
+    float: none;
+    margin: 10px auto;
+  }
+  .direct-contact-container {
+    margin-top: 60px;
+    max-width: 300px;
+  }
+  .social-media-list li {
+    height: 60px;
+    width: 60px;
+    line-height: 60px;
+  }
+  .social-media-list li:after {
+    width: 60px;
+    height: 60px;
+    line-height: 60px;
+  }
+}
+
+@media screen and (max-width: 569px) {
+  #contact {
+    height: 1200px;
+  }
+  .section-header {
+    font-size: 50px;
+  }
+  .direct-contact-container,
+  .form-wrapper {
+    float: none;
+    margin: 0 auto;
+  }
+  .form-control,
+  textarea {
+    max-width: 340px;
+    margin: 0 auto;
+  }
+
+  .name,
+  .email,
+  textarea {
+    width: 280px;
+  }
+  .direct-contact-container {
+    margin-top: 60px;
+    max-width: 280px;
+  }
+  .social-media-list {
+    left: 0;
+  }
+  .social-media-list li {
+    height: 55px;
+    width: 55px;
+    line-height: 55px;
+    font-size: 2rem;
+  }
+  .social-media-list li:after {
+    width: 55px;
+    height: 55px;
+    line-height: 55px;
+  }
+}
+
+@media screen and (max-width: 410px) {
+  .send-button {
+    width: 99%;
+  }
+}
+
+  </style>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-L179PQJHZW"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'G-L179PQJHZW');
+		</script>
+</head>
+<body>
+<section id="contact">
+
+  <h1 class="section-header">CONTACT</h1>
+
+  <div class="contact-wrapper">
+
+    <!---------------- 
+
+      CONTACT PAGE LEFT 
+    
+      ----------------->
+
+    <form action="#" method="post" class="col-md-6">
+                                 <div class=" ">
+                                    <div class=" form-group">
+                                       <input class=" form-control" type="text" placeholder="Your Name" name="name" required="">
+                                    </div>
+                                    <div class=" form-group">
+                                       <input class=" form-control" type="email" placeholder="Your Email" name="email" required="">
+                                    </div>
+                                    <div class=" form-group">
+                                       <input class=" form-control" type="text" placeholder="Phone Number" name="telephone" required="">
+                                    </div>
+                                    <div class=" form-group">
+                                       <textarea class=" form-control" name="comments" placeholder="Message..." required=""></textarea>
+                                    </div>
+                                    <!--<input type="submit" value="Submit" name="submit">-->
+                                    <input type="submit" value="Submit" class="submit-btn form-control btn btn-default submit_btn"> 
+                                 </div>
+                              </form>
+							  
+    <!---------------- 
+
+      CONTACT PAGE RIGHT 
+    
+      ----------------->
+
+    <div class="direct-contact-container col-md-6">
+
+      <ul class="contact-list">
+        <li class="list-item"><i class="fa fa-map-marker fa-2x"><span class="contact-text place">Ahmedabad Gujarat </span></i></li>
+
+        <li class="list-item"><i class="fa fa-phone fa-2x"><span class="contact-text phone"><a href="tel:+91 8511511038" title="Give me a call">+91 8511511038</a></span></i></li>
+
+        <li class="list-item"><i class="fa fa-envelope fa-2x"><span class="contact-text gmail"><a href="mailto:mthumar85@gmail.com
+" title="Send me an email">mthumar85@gmail.com
+</a></span></i></li>
+
+      </ul>
+
+      <hr>
+      
+      <hr>
+
+      <div class="copyright">&copy; ALL OF THE RIGHTS RESERVED</div>
+
+    </div>
+
+  </div>
+ 
+</section>
+   <?php
+                                 if(isset($_POST['email'])) {
+                                  
+                                     // EDIT THE 2 LINES BELOW AS REQUIRED
+                                     $email_to = "mthumar85@gmail.com";
+                                     $email_subject = "contact enquiry from textconverts.com";
+                                  
+                                     function died($error) {
+                                         // your error code can go here
+                                         echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+                                         echo "These errors appear below.<br /><br />";
+                                         echo $error."<br /><br />";
+                                         echo "Please go back and fix these errors.<br /><br />";
+                                         die();
+                                     }
+                                  
+                                  
+                                     // validation expected data exists
+                                     if(!isset($_POST['name']) ||
+                                         
+                                         !isset($_POST['email']) ||
+                                         !isset($_POST['telephone']) ||
+                                         !isset($_POST['comments'])) {
+                                         died('We are sorry, but there appears to be a problem with the form you submitted.');       
+                                     }
+                                  
+                                      
+                                  
+                                     $name = $_POST['name']; // required
+                                   
+                                     $email_from = $_POST['email']; // required
+                                     $telephone = $_POST['telephone']; // not required
+                                     $comments = $_POST['comments']; // required
+                                  
+                                     $error_message = "";
+                                     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+                                  
+                                   if(!preg_match($email_exp,$email_from)) {
+                                     $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+                                   }
+                                  
+                                     $string_exp = "/^[A-Za-z .'-]+$/";
+                                  
+                                   if(!preg_match($string_exp,$name)) {
+                                     $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+                                   }
+                                  
+                                 
+                                   if(strlen($comments) < 2) {
+                                     $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+                                   }
+                                  
+                                   if(strlen($error_message) > 0) {
+                                     died($error_message);
+                                   }
+                                  
+                                     $email_message = "Form details below.\n\n";
+                                  
+                                      
+                                     function clean_string($string) {
+                                       $bad = array("content-type","bcc:","to:","cc:","href");
+                                       return str_replace($bad,"",$string);
+                                     }
+                                  
+                                      
+                                  
+                                     $email_message .= "First Name: ".clean_string($name)."\n";
+                                    
+                                     $email_message .= "Email: ".clean_string($email_from)."\n";
+                                     $email_message .= "Telephone: ".clean_string($telephone)."\n";
+                                     $email_message .= "Comments: ".clean_string($comments)."\n";
+                                  
+                                 // create email headers
+                                 $headers = 'From: '.$email_from."\r\n".
+                                 'Reply-To: '.$email_from."\r\n" .
+                                 'X-Mailer: PHP/' . phpversion();
+                                 @mail($email_to, $email_subject, $email_message, $headers); 
+                                 
+                                  
+                                 ?>
+                              <div class="alert alert-success" role="alert">
+                                 <p>Your Email is Send</p>
+                              </div>
+                              <!-- include your own success html here -->
+                              <?php
+                                 }
+                                 ?>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/602616c09c4f165d47c28a1e/1euachocd';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
